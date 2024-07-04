ec38a1bed0aae3d5a9b73e3c8e83bcdcd5e0c535
kamil.f <kamil.f@flipside.team>
2024-07-04 21:24:34 +0200

    Added prototype of match model

--- tesbet/bets/models.py
+++ tesbet/bets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Match(models.Model):
+    begin_at = models.DateTimeField(blank=False)
+    name = models.TextField(blank=False, max_length=250)
+    league_id = models.IntegerField(blank=False)
